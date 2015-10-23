eeb73133e12bd7a856aed253426621a1e5637590
maryjillpeterson <pet12021@byui.edu>
2015-10-22 21:54:05 -0600

    Oct 22, 2015 - Team Assignment upload.

--- Revenge/test/byui/cit260/revenge/control/battleControlTest.java
+++ Revenge/test/byui/cit260/revenge/control/battleControlTest.java
@@ -45,9 +45,52 @@ public class battleControlTest {
         double expResult = 12.0;
         battleControl instance = new battleControl();
         double result = instance.calculateHealth(playerStrength, damage);
-        assertEquals(expResult,result,0.01);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        assertEquals(expResult,result,12.0);
+        
+        /**************************
+        *Test case #2        
+        **************************/
+        
+        System.out.println("\tTest case #2");
+        playerStrength = -2.0;
+        damage = 1.0;
+        expResult = -1.0;
+        result = instance.calculateHealth(playerStrength, damage);
+        assertEquals(expResult, result, -1.0);
     
+        /**************************
+        *Test case #3        
+        **************************/
+        
+        System.out.println("\tTest case #3");
+        playerStrength = 25;
+        damage = 4;
+        expResult = 21;
+        result = instance.calculateHealth(playerStrength, damage);
+        assertEquals(expResult, result, 21);
+    
+        /**************************
+        *Test case #4        
+        **************************/
+        
+        System.out.println("\tTest case #4");
+        playerStrength = 20;
+        damage = 0;
+        expResult = 20;
+        result = instance.calculateHealth(playerStrength, damage);
+        assertEquals(expResult, result, 20);
+    
+        /**************************
+        *Test case #5        
+        **************************/
+        
+        System.out.println("\tTest case #5");
+        playerStrength = 0;
+        damage = 5;
+        expResult = -5;
+        result = instance.calculateHealth(playerStrength, damage);
+        assertEquals(expResult, result, -5);
+
+       
     }
 }
