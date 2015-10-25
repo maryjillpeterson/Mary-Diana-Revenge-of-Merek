c3c97c2fa83aac26d55277134ec014fb9458c2db
maryjillpeterson <pet12021@byui.edu>
2015-10-25 00:43:40 -0600

    Oct 22, 2015 - Team Assignment upload.

--- Revenge/src/byui/cit260/revenge/control/questControl.java
+++ Revenge/src/byui/cit260/revenge/control/questControl.java
@@ -11,5 +11,18 @@ package byui.cit260.revenge.control;
  * @author chadepeterson
  */
 public class questControl {
-    
+    public double calculateVolume(double radius, double height){
+       if (radius <= 12 && radius >= 24){
+            return -1;
+        }
+        
+        if (height <= 40 && height >= 60){
+            return -1;
+        }
+        
+        double volume = 3.14 * radius * radius * height;
+        
+        return volume; 
+        
+} 
 }
